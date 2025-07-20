c3f3e27f259aef3aeb1e53c2ab18536618894e52
Peter Gastinger <peter.gastinger@gmail.com>
2025-07-20 13:00:44 +0200

    update

--- django_htmx_project/settings/dev.py
+++ django_htmx_project/settings/dev.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Third-party apps
     'django_htmx',
-    'template_partials',
     # Local apps
     'tasks',    
 ]
